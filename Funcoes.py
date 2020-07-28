780dad8c4178c3469b3218ebd1fa1dab1093e9d3
jvitorcardoso <aspirantevitor@gmail.com>
2020-07-28 16:45:09 -0300

    Add files via upload

---
+++ Funcoes.py
@@ -0,0 +1,32 @@
+from math import sqrt
+
+def eq_2_grau(a, b, c):
+    x = 1
+    y_x = (a * (x ** 2)) + (b * x) + c
+    delta = (b ** 2) - 4 * a * c
+    x_1 = ((-b) + sqrt(delta)) / (2 * a)
+    x_2 = ((-b) - sqrt(delta)) / (2 * a)
+    x_real = (-b) / (2 * a)
+    if b < 0:
+        print(f'y(x) = {a:.0f}x^2 {b:.0f}x + {c:.0f} = 0')
+    else:
+        print(f'y(x) = {a:.0f}x^2 - {b:.0f}x + {c:.0f} = 0')
+    if delta == 0:
+        print(f'DELTA = {b:.0f}^2 - 4*{a:.0f}*{c:.0f}')
+        print(f'DELTA = {delta:.0f} (POSSUI 1 RAIZ)')
+        print(f'RAIZ = {x_real}')
+    elif delta < 0:
+        print(f'DELTA = {b:.0f}^2 - 4*{a:.0f}*{c:.0f}')
+        print(f'DELTA = {delta:.0f}')
+        print('DELTA NEGATIVO, NÃO POSSUI RAIZ')
+    elif delta > 0:
+        print(f'DELTA = {b:.0f}^2 - 4*{a:.0f}*{c:.0f}')
+        print(f'DELTA = {delta:.0f} (POSSUI 2 RAÍZES)')
+        print(f'RAIZ 1 = {x_1:.0f}')
+        print(f'RAIZ 2 = {x_2:.0f}')
+    return y_x
+
+a = float(input("Enter A: "))
+b = float(input("Enter B: "))
+c = float(input("Enter C: "))
+eq_2_grau(a, b, c)
---
+++ Hello World.py
@@ -0,0 +1,7 @@
+a = input("Entre com um dado: ")
+print("É uma palavra? ", a.isalpha())
+print("É um número? ", a.isnumeric())
+print("Contém apenas espaços? ", a.isspace())
+print("Está capitalizado? ", a.istitle())
+print("Contém apenas minúsculas? ", a.islower())
+print("Contém apenas maiúsculas?", a.isupper())
