c9295a1bd604056fec09b015d946ccbf7f448e99
mhscardoso <34404270+mhscardoso@users.noreply.github.com>
2020-07-29 12:28:15 -0300

    Update field.py

--- field.py
+++ field.py
@@ -2,44 +2,66 @@
 # Lembrando que uma função de grau n sempre terá n raízes que poderão ser reais ou imaginárias. Portanto, uma função do segundo grau sempre possui 2(duas) raízes
 # Aqui, o código mostra as raízes caso forem imaginárias, lembrando que j é a unidade imaginária. (j = sqrt(-1))
 
+
+# A primeira para obter as raízes de números positivos - reais. A segunda, para obter a raíz de números negativos - imaginários.
 from math import sqrt
 import cmath
 
+# A função recebe os valores a, b e c, que são os coeficientes da equação do segundo grau.
 def f2g(a, b, c):
-
-    print(f'A função é da forma: {a:.0f}x2 + {b:.0f}x + {c:.0f}')
     
-    d = (b**2) - (4 * a * c)
-
+    # Verifica os valores inseridos na função.
     try:
-        root = sqrt(d)
-    except:
-        root = cmath.sqrt(d)
-    
-    x1 = (-b + root) / 2
-    x2 = (-b - root) / 2
+        a = float(a)
+        b = float(b)
+        c = float(c)
+        k = -b/a
 
-    if d > 0:
-        print('A função possui duas raízes reais diferentes: ')
-        print(f'x1 = {x1:.2f}')
-        print(f'x2 = {x2:.2f}')    
-    elif d == 0:
-        print('A função possui duas raízes iguais: ')
-        print(f'x1 = x2 = {x1:.2f}')
+    # Aqui, verifica se o grau da função é, de fato 2, como deve ser.    
+    except ZeroDivisionError:
+        print('O coeficiente líder não pode ser nulo.')
+        return None
+    
+    # Rejeita os erros de valores.
+    except:
+        print('Os valores digitados são inválidos.')
+        return None
     
     else:
-        print('A função possui duas raízes imaginárias:')
-        print(f'x1 = {x1:.2f}')
-        print(f'x2 = {x2:.2f}')
-    
-    return x1, x2
+        # Mostra a forma da função.
+        print(f'A função é da forma: {a:.0f}x2 + {b:.0f}x + {c:.0f}')
 
+        # Calcula o delta da equação.
+        d = (b**2) - (4 * a * c)
 
-a = float(input('Coeficiente líder: '))
-b = float(input('Coeficiente segundo: '))
-c = float(input('Coeficiente independente: '))
+        # Tenta calcular a raíz de delta, se não for possível, d < 0 e, portanto, será necessário utilizar números complexos com a biblioteca cmath.
+        try:
+            root = sqrt(d)
+        except:
+            root = cmath.sqrt(d)
 
-f2g(a, b, c)
+        # Calcula, enfim as raízes.        
+        x1 = (-b + root) / (2 * a)
+        x2 = (-b - root) / (2 * a)
+
+        # Mostra para o usuário as três possíveis situações. Com d > 0 - duas raízes reais distintas - d = 0 - duas raízes iguais - e d < 0 - duas raízes imaginárias.
+        if d > 0:
+            print('A função possui duas raízes reais diferentes: ')
+            print(f'x1 = {x1:.2f}')
+            print(f'x2 = {x2:.2f}')    
+
+        elif d == 0:
+            print('A função possui duas raízes iguais: ')
+            print(f'x1 = x2 = {x1:.2f}')
+
+        else:
+            print('A função possui duas raízes imaginárias:')
+            print(f'x1 = {x1:.2f}')
+            print(f'x2 = {x2:.2f}')
+
+        # Retorna os valores das raízes.
+        return x1, x2
+
 
 
 
